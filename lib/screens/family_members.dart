7c141135a9c377a799125881902c031204b52a47
Yomna254 <150300298+Yomna254@users.noreply.github.com>
2024-10-22 02:10:25 +0300

    Update family_members.dart

--- lib/screens/family_members.dart
+++ lib/screens/family_members.dart
@@ -6,52 +6,52 @@ class FamilyMembers extends StatelessWidget {
   const FamilyMembers({super.key});
   final List<Number> numbers = const [
     Number(
-        sound: 'sounds/numbers/number_one_sound - Copy.wav',
+        sound: 'sounds/family_members/father.wav',
         image: 'assets/images/family_members/family_father.png',
         enName: 'father',
         jpName: 'Chichioya'),
     Number(
-        sound: 'sounds/numbers/number_two_sound.mp3',
+        sound: 'sounds/family_members/mother.wav',
         image: 'assets/images/family_members/family_mother.png',
         enName: 'mother',
         jpName: 'Hahaoya'),
     Number(
-        sound: 'sounds/numbers/number_two_sound.mp3',
+        sound: 'sounds/family_members/daughter.wav',
         image: 'assets/images/family_members/family_daughter.png',
         enName: 'daughter',
         jpName: 'Musume'),
     Number(
-        sound: 'sounds/numbers/number_two_sound.mp3',
+        sound: 'sounds/family_members/son.wav',
         image: 'assets/images/family_members/family_son.png',
         enName: 'son',
         jpName: 'Musuko'),
     Number(
-        sound: 'sounds/numbers/number_two_sound.mp3',
+        sound: 'sounds/family_members/grand father.wav',
         image: 'assets/images/family_members/family_grandfather.png',
         enName: 'grand father',
         jpName: 'Sofu'),
     Number(
-        sound: 'sounds/numbers/number_two_sound.mp3',
+        sound: 'sounds/family_members/grand mother.wav',
         image: 'assets/images/family_members/family_grandmother.png',
         enName: 'grand mother',
         jpName: 'Sobo'),
     Number(
-        sound: 'sounds/numbers/number_two_sound.mp3',
+        sound: 'sounds/family_members/older brother.wav',
         image: 'assets/images/family_members/family_older_brother.png',
         enName: 'older brother',
         jpName: 'Ani'),
     Number(
-        sound: 'sounds/numbers/number_two_sound.mp3',
+        sound: 'sounds/family_members/older sister.wav',
         image: 'assets/images/family_members/family_older_sister.png',
         enName: 'older sister',
         jpName: 'Ane'),
     Number(
-        sound: 'sounds/numbers/number_two_sound.mp3',
+        sound: 'sounds/family_members/younger brother.wav',
         image: 'assets/images/family_members/family_younger_brother.png',
         enName: 'younger brother',
         jpName: 'Otōto'),
     Number(
-        sound: 'sounds/numbers/number_two_sound.mp3',
+        sound: 'sounds/younger sister',
         image: 'assets/images/family_members/family_younger_sister.png',
         enName: 'younger sister',
         jpName: 'Imōto'),
